6c97d910a13b0dfb5227d8d417934c286ef4bb7d
KathrynForrest02 <b01841363@studentmail.uws.ac.uk>
2026-02-22 12:37:32 +0000

    Fix merge conflict in test_app.py

--- flask-app/tests/test_app.py
+++ flask-app/tests/test_app.py
@@ -1,8 +1,6 @@
 from app import calculate_race_times, format_time
-<<<<<<< HEAD
 
-=======
->>>>>>> 8c74803 (initial commit)
+
 def test_format_time():
     assert format_time(0) == "0:00"
     assert format_time(1) == "1:00"
@@ -10,21 +8,15 @@ def test_format_time():
     assert format_time(60) == "1:00:00"
     assert format_time(181) == "3:01:00"
     assert format_time(611) == "10:11:00"
-<<<<<<< HEAD
 
-=======
->>>>>>> 8c74803 (initial commit)
+
 def test_calculate_race_times():
     # pace per km is 5 min
-    #TODO: complete the dict with the expected times
     distance_result = {
         "5K": "25:00",
         "10K": "50:00",
         "Half Marathon": "1:45:29",
         "Marathon": "3:30:58"
     }
-<<<<<<< HEAD
 
-=======
->>>>>>> 8c74803 (initial commit)
     assert calculate_race_times(5) == distance_result
